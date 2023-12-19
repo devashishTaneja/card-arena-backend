c77bd0ab3edef176b3f101b2133724b28d03dbe0
Devashish Taneja <dtaneja@enphaseenergy.com>
2023-12-20 02:18:44 +0530

    Initial commit

---
+++ src/test/java/com/cardarena/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardarena.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
